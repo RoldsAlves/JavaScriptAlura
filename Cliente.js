623e3db7a55e91f313fe82cc7aa9d5bc46b83804
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2022-02-06 23:57:49 -0300

    Interfaces e Herança em Orientação a Objetos - Interfaces e Duck Type

--- Cliente.js
+++ Cliente.js
@@ -6,10 +6,10 @@ export class Cliente {
     constructor(nome, cpf, senha){
         this.nome = nome;
         this._cpf = cpf;
-        this._senha =senha
+        this._senha = senha;
     }
 
-    autenticar(senha) {
-        return this._senha == senha;
+    autenticar() {
+        return true;
     }
 }
