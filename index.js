d5acbc03edd2faddf78db08261526f69a117dab9
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2022-02-06 23:47:20 -0300

    Interfaces e Herança em Orientação a Objetos - Sistema Interno

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Rodolfo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const diretor = new Diretor("Rodolfo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Lorena", 5000, 83364499900);
+gerente.cadastrarSenha("123456");
+const cliente = new Cliente("Lais", 79946513100, "467898");
 
-console.log(cliente1);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "467898");
 
-const conta1 = new ContaCorrente(cliente1, 1001);
-conta1.depositar(500);
-conta1.sacar(100);
-const conta2 = new ContaPoupanca(150, cliente2, 1001);
-conta2.sacar(50);
 
-const conta3 = new ContaSalario(cliente1);
-conta3.depositar(1000);
-conta3.sacar(100);
-
-console.log(conta1);
-console.log(conta2);
-console.log(conta3);
+console.log(diretorEstaLogado, gerenteEstaLogado, clienteEstaLogado);
